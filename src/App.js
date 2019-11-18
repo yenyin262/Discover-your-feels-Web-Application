ba99bf921c71c9ba34f39bf47b4a990e322b9631
Yen Yin Hing <yenyinf287@gmail.com>
2019-11-18 15:06:28 -0800

    add home component anad remove search component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import SpotifySignIn from "./Components/SpotifySignIn";
 import ViewPlaylistPage from "./Components/ViewPlaylistPage";
 import "./App.css";
 import PlaylistResult from "./Components/PlaylistResult";
-import SearchBar from "./Components/SearchBar";
+import Home from "./Components/Home";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <div>
-          <SearchBar />
           <Route path="/" exact component={SpotifySignIn} />
+          <Route path="/Home" exact component={Home} />
           <Route
             path="/PlaylistResult/:query"
             exact
